2089bfe9b627bd26f23e84eeaace84b3d0374e70
DanDDevelopment <68516575+DanDDevelopment@users.noreply.github.com>
2024-06-01 12:30:37 -0700

    Minimap zoom fix

--- DynMain/QBCoreFramework_59E731.base/resources/[dyn]/abl-minimap-part5-OP/Files/CL.lua
+++ DynMain/QBCoreFramework_59E731.base/resources/[dyn]/abl-minimap-part5-OP/Files/CL.lua
@@ -24,27 +24,24 @@ local replacement = {
 ------- THREADS -------
 ---------------------------
 
-CreateThread(function()
-  SetMapZoomDataLevel(0, 2.75, 0.9, 0.08, 0.0, 0.0) -- Level 0
-  SetMapZoomDataLevel(1, 2.8, 0.9, 0.08, 0.0, 0.0)  -- Level 1
-  SetMapZoomDataLevel(2, 8.0, 0.9, 0.08, 0.0, 0.0)  -- Level 2
-  SetMapZoomDataLevel(3, 20.0, 0.9, 0.08, 0.0, 0.0) -- Level 3
-  SetMapZoomDataLevel(4, 35.0, 0.9, 0.08, 0.0, 0.0) -- Level 4
-  SetMapZoomDataLevel(5, 55.0, 0.0, 0.1, 2.0, 1.0)  -- ZOOM_LEVEL_GOLF_COURSE
-  SetMapZoomDataLevel(6, 450.0, 0.0, 0.1, 1.0, 1.0) -- ZOOM_LEVEL_INTERIOR
-  SetMapZoomDataLevel(7, 4.5, 0.0, 0.0, 0.0, 0.0)   -- ZOOM_LEVEL_GALLERY
-  SetMapZoomDataLevel(8, 11.0, 0.0, 0.0, 2.0, 3.0)  -- ZOOM_LEVEL_GALLERY_MAXIMIZE
+Citizen.CreateThread(function()
+  SetMapZoomDataLevel(0, 2.75, 0.9, 0.08, 0.0, 0.0)
+  SetMapZoomDataLevel(1, 2.8, 0.9, 0.08, 0.0, 0.0)
+  SetMapZoomDataLevel(2, 8.0, 0.9, 0.08, 0.0, 0.0)
+  SetMapZoomDataLevel(3, 20.0, 0.9, 0.08, 0.0, 0.0)
+  SetMapZoomDataLevel(4, 35.0, 0.9, 0.08, 0.0, 0.0)
+  SetMapZoomDataLevel(5, 55.0, 0.0, 0.1, 2.0, 1.0)
+  SetMapZoomDataLevel(6, 450.0, 0.0, 0.1, 1.0, 1.0)
+  SetMapZoomDataLevel(7, 4.5, 0.0, 0.0, 0.0, 0.0)
+  SetMapZoomDataLevel(8, 11.0, 0.0, 0.0, 2.0, 3.0)
 end)
 
-CreateThread(function()
+Citizen.CreateThread(function()
   while true do
-    Wait(80)
-    local ped = PlayerPedId()
-    if IsPedOnFoot(ped) then
-      SetRadarZoom(1200) -- cyan 1100
-    elseif IsPedInAnyVehicle(ped, true) then
-      SetRadarZoom(1200) -- cyan 1100
-    end
+    Wait(1000)
+    local playerPed = PlayerPedId()
+    local currentZoomSetting = IsPedOnFoot(playerPed) and 1100 or nil
+    SetRadarZoom(currentZoomSetting or 1100)
   end
 end)
 
